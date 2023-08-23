32500a80b25a2bdc1bf8b375a629a10ae4ecfeed
Robert Skalko <treborx555@gmail.com>
2023-08-23 11:49:27 +0200

    automate stat serializers

--- src/main/java/com/robertx22/age_of_exile/uncommon/effectdatas/rework/condition/StatCondition.java
+++ src/main/java/com/robertx22/age_of_exile/uncommon/effectdatas/rework/condition/StatCondition.java
@@ -6,6 +6,7 @@ import com.robertx22.age_of_exile.database.registry.ExileRegistryTypes;
 import com.robertx22.age_of_exile.saveclasses.unit.StatData;
 import com.robertx22.age_of_exile.uncommon.effectdatas.EffectEvent;
 import com.robertx22.age_of_exile.uncommon.interfaces.EffectSides;
+import com.robertx22.library_of_exile.registry.Database;
 import com.robertx22.library_of_exile.registry.ExileRegistryType;
 import com.robertx22.library_of_exile.registry.IAutoGson;
 import com.robertx22.library_of_exile.registry.JsonExileRegistry;
@@ -17,31 +18,10 @@ public abstract class StatCondition implements JsonExileRegistry<StatCondition>,
     public static StatCondition SERIALIZER = new RandomRollCondition();
     public static HashMap<String, StatCondition> SERIALIZERS = new HashMap<>();
 
-    static {
-        addSer(new RequireChargedAttack());
-        addSer(new IsHealthAbovePercentCondition());
-        addSer(new IsUndeadCondition());
-        addSer(new IsHealthBellowPercentCondition());
-        addSer(new EffectHasTagCondition());
-        addSer(new IsInCombatCondition());
-        addSer(new IsNotOnCooldownCondition());
-        addSer(new IsUnderExileEffect());
-        addSer(new EitherIsTrueCondition());
-        addSer(new IsDayCondition());
-        addSer(new IsSpellCondition());
-        addSer(new SpellHasTagCondition());
-        addSer(new IsBooleanTrueCondition());
-        addSer(new RandomRollCondition());
-        addSer(new ElementMatchesStat());
-        addSer(new StringMatchesCondition());
-        addSer(new WeaponTypeMatches());
-        addSer(new LightLevelCondition());
-        addSer(new IsTargetCursed());
-        
-    }
 
-    static void addSer(StatCondition eff) {
+    public static void addSer(StatCondition eff) {
         SERIALIZERS.put(eff.ser, eff);
+
     }
 
     public String id = "";
@@ -61,6 +41,12 @@ public abstract class StatCondition implements JsonExileRegistry<StatCondition>,
         this.is = false;
         this.id += "_is_false";
         return this;
+    }
+
+    @Override
+    public void addToSerializables() {
+        Database.getRegistry(this.getExileRegistryType()).addSerializable(this);
+        addSer(this);
     }
 
     public abstract boolean can(EffectEvent event, EffectSides statSource, StatData data, Stat stat);
