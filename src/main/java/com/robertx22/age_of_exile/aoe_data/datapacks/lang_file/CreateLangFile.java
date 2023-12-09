e98c4f23b913aa51f14fa4d29e4210b6f0d06f3c
TUsama <1716976420@qq.com>
2023-12-09 18:22:18 +0800

    Update CreateLangFile.java

--- src/main/java/com/robertx22/age_of_exile/aoe_data/datapacks/lang_file/CreateLangFile.java
+++ src/main/java/com/robertx22/age_of_exile/aoe_data/datapacks/lang_file/CreateLangFile.java
@@ -153,9 +153,6 @@ public class CreateLangFile {
         list.addAll(Arrays.asList(GemItem.GemType.values()));
         list.addAll(Arrays.asList(GemItem.GemRank.values()));
         list.addAll(Arrays.asList(PlayerBuffData.Type.values()));
-        //
-        list.addAll(Arrays.asList(new AscendancyClass()));
-
 
 
         HashMap<IAutoLocName.AutoLocGroup, List<IAutoLocName>> map = new HashMap<>();
