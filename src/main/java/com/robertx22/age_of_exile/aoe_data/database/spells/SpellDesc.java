b18b7f61061f066d28d8d488fc12ca76518cec4d
TUsama <1716976420@qq.com>
2023-12-20 14:11:46 +0800

    fix

--- src/main/java/com/robertx22/age_of_exile/aoe_data/database/spells/SpellDesc.java
+++ src/main/java/com/robertx22/age_of_exile/aoe_data/database/spells/SpellDesc.java
@@ -3,11 +3,11 @@ package com.robertx22.age_of_exile.aoe_data.database.spells;
 import com.robertx22.age_of_exile.database.data.spells.components.Spell;
 import com.robertx22.age_of_exile.database.data.value_calc.ValueCalculation;
 import com.robertx22.age_of_exile.database.registry.ExileDB;
+import com.robertx22.age_of_exile.uncommon.utilityclasses.StringUTIL;
 import com.robertx22.age_of_exile.uncommon.utilityclasses.TooltipUtils;
 import com.robertx22.library_of_exile.utils.CLOC;
 import net.minecraft.ChatFormatting;
 import net.minecraft.world.entity.LivingEntity;
-import org.codehaus.plexus.util.StringUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,7 +21,7 @@ public class SpellDesc {
         String tip = CLOC.translate(spell.locDesc());
         String copy = tip;
 
-        int amount = StringUtils.countMatches(tip, "calc:");
+        int amount = StringUTIL.countMatches(tip, "calc:");
 
         int counted = 0;
         for (ValueCalculation calc : ExileDB.ValueCalculations()
