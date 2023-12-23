fa1446ddf0319ce0aa2c329f4c8ebe62714f1de3
Robert Skalko <treborx555@gmail.com>
2023-12-23 13:25:54 +0100

    trying to rewrite calc code a bit..

--- src/main/java/com/robertx22/age_of_exile/capability/entity/EntityData.java
+++ src/main/java/com/robertx22/age_of_exile/capability/entity/EntityData.java
@@ -6,7 +6,6 @@ import com.robertx22.age_of_exile.config.forge.ServerContainer;
 import com.robertx22.age_of_exile.database.data.EntityConfig;
 import com.robertx22.age_of_exile.database.data.game_balance_config.GameBalanceConfig;
 import com.robertx22.age_of_exile.database.data.gear_slots.GearSlot;
-import com.robertx22.age_of_exile.database.data.gear_types.bases.BaseGearType;
 import com.robertx22.age_of_exile.database.data.mob_affixes.MobAffix;
 import com.robertx22.age_of_exile.database.data.rarities.MobRarity;
 import com.robertx22.age_of_exile.database.data.stats.StatScaling;
@@ -15,12 +14,15 @@ import com.robertx22.age_of_exile.database.data.stats.types.resources.energy.Ene
 import com.robertx22.age_of_exile.database.data.stats.types.resources.health.Health;
 import com.robertx22.age_of_exile.database.registry.ExileDB;
 import com.robertx22.age_of_exile.event_hooks.damage_hooks.util.AttackInformation;
-import com.robertx22.age_of_exile.event_hooks.my_events.CollectGearEvent;
 import com.robertx22.age_of_exile.event_hooks.player.OnLogin;
 import com.robertx22.age_of_exile.mmorpg.SlashRef;
 import com.robertx22.age_of_exile.saveclasses.CustomExactStatsData;
 import com.robertx22.age_of_exile.saveclasses.item_classes.GearItemData;
-import com.robertx22.age_of_exile.saveclasses.unit.*;
+import com.robertx22.age_of_exile.saveclasses.unit.MobData;
+import com.robertx22.age_of_exile.saveclasses.unit.ResourceType;
+import com.robertx22.age_of_exile.saveclasses.unit.ResourcesData;
+import com.robertx22.age_of_exile.saveclasses.unit.Unit;
+import com.robertx22.age_of_exile.saveclasses.unit.stat_calc.StatCalculation;
 import com.robertx22.age_of_exile.uncommon.MathHelper;
 import com.robertx22.age_of_exile.uncommon.datasaving.CustomExactStats;
 import com.robertx22.age_of_exile.uncommon.datasaving.Load;
@@ -509,7 +511,7 @@ public class EntityData implements ICap, INeededForClient {
 
         if (needsToRecalcStats()) {
             //Watch watch = new Watch();
-            unit.recalculateStats(entity, this, null, -1);
+            this.unit = StatCalculation.calc(entity, -1, null);
 
             if (entity instanceof Player p) {
                 var data = Load.player(p);
@@ -520,16 +522,14 @@ public class EntityData implements ICap, INeededForClient {
 
     }
 
-    public void forceRecalculateStats(AttackInformation data) {
-        unit.recalculateStats(entity, this, data, -1);
-    }
 
     public void forceRecalculateStats() {
 
         if (unit == null) {
             unit = new Unit();
         }
-        unit.recalculateStats(entity, this, null, -1);
+        this.unit = StatCalculation.calc(entity, -1, null);
+      
     }
 
     // This reduces stat calculation by about 4 TIMES!
@@ -816,34 +816,6 @@ public class EntityData implements ICap, INeededForClient {
         return level;
     }
 
-    public int getAverageILVL() {
-
-        List<GearData> gears = new ArrayList<>();
-        new CollectGearEvent.CollectedGearStacks(entity, gears, null);
-
-        int divideBy = 4 + 3; // armors, jewelry
-
-        // dont count weapon and offhand for obvious reasons
-
-        int totalILVL = 0;
-
-        for (GearData x : gears) {
-            if (x.gear != null) {
-                if (!x.gear.GetBaseGearType()
-                        .isWeapon() && !x.gear.GetBaseGearType().tags.contains(BaseGearType.SlotTag.offhand_family)) {
-                    totalILVL += x.gear.getLevel();
-                }
-            }
-        }
-
-        if (totalILVL < 1) {
-            return 0;
-        }
-
-        totalILVL /= divideBy;
-
-        return totalILVL;
-    }
 
     public void setLevel(int lvl) {
 
--- src/main/java/com/robertx22/age_of_exile/capability/player/data/StatPointsData.java
+++ src/main/java/com/robertx22/age_of_exile/capability/player/data/StatPointsData.java
@@ -3,33 +3,28 @@ package com.robertx22.age_of_exile.capability.player.data;
 import com.robertx22.age_of_exile.database.data.game_balance_config.GameBalanceConfig;
 import com.robertx22.age_of_exile.database.registry.ExileDB;
 import com.robertx22.age_of_exile.saveclasses.ExactStatData;
-import com.robertx22.age_of_exile.saveclasses.unit.Unit;
+import com.robertx22.age_of_exile.saveclasses.gearitem.gear_bases.IStatCtx;
+import com.robertx22.age_of_exile.saveclasses.unit.stat_ctx.MiscStatCtx;
+import com.robertx22.age_of_exile.saveclasses.unit.stat_ctx.StatContext;
 import com.robertx22.age_of_exile.uncommon.datasaving.Load;
 import com.robertx22.age_of_exile.uncommon.enumclasses.ModType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
-public class StatPointsData {
+public class StatPointsData implements IStatCtx {
 
 
     public HashMap<String, Integer> map = new HashMap<>();
 
     public void reset() {
         this.map.clear();
-    }
-
-    public void addStats(Unit unit) {
-        map.entrySet()
-                .forEach(x -> {
-                    float val = x.getValue();
-                    ExactStatData stat = ExactStatData.levelScaled(val, ExileDB.Stats()
-                            .get(x.getKey()), ModType.FLAT, 1);
-                    stat.applyToStatInCalc(unit);
-                });
-
     }
 
     public int getFreePoints(Player player) {
@@ -43,5 +38,18 @@ public class StatPointsData {
         }
 
         return total - spent;
+    }
+
+    @Override
+    public List<StatContext> getStatAndContext(LivingEntity en) {
+
+        var list = map.entrySet().stream().map(x -> {
+            float val = x.getValue();
+            ExactStatData stat = ExactStatData.levelScaled(val, ExileDB.Stats()
+                    .get(x.getKey()), ModType.FLAT, 1);
+            return stat;
+        }).collect(Collectors.toList());
+
+        return Arrays.asList(new MiscStatCtx(list));
     }
 }
