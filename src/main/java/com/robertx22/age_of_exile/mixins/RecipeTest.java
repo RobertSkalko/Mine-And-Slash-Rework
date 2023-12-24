3e0fa7b67bbe39f5ee881054dcc74dcb7418b195
Robert Skalko <treborx555@gmail.com>
2023-12-25 00:05:58 +0100

    test

---
+++ src/main/java/com/robertx22/age_of_exile/mixins/RecipeTest.java
@@ -0,0 +1,69 @@
+package com.robertx22.age_of_exile.mixins;
+
+import net.minecraft.stats.RecipeBook;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(RecipeBook.class)
+public class RecipeTest {
+
+    // todo this seems to work, try in JEI and if JEI works too make this a separate mod
+    // NO IDEA how dumb this is, could break things but if not, it might be solve the big nbt problem
+
+    /*
+
+    @Inject(method = "add(Lnet/minecraft/world/item/crafting/Recipe;)V", cancellable = true, at = @At(value = "HEAD"))
+    public void hookDisableFire(Recipe<?> pRecipe, CallbackInfo ci) {
+        try {
+            ci.cancel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Inject(method = "add(Lnet/minecraft/resources/ResourceLocation;)V", cancellable = true, at = @At(value = "HEAD"))
+    public void hookDisableFire(ResourceLocation pRecipeId, CallbackInfo ci) {
+        try {
+            ci.cancel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Inject(method = "addHighlight(Lnet/minecraft/world/item/crafting/Recipe;)V", cancellable = true, at = @At(value = "HEAD"))
+    public void hookDisdsableFire(Recipe<?> pRecipe, CallbackInfo ci) {
+        try {
+            ci.cancel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Inject(method = "addHighlight(Lnet/minecraft/resources/ResourceLocation;)V", cancellable = true, at = @At(value = "HEAD"))
+    public void hookDisdsableFire(ResourceLocation pRecipeId, CallbackInfo ci) {
+        try {
+            ci.cancel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Inject(method = "contains(Lnet/minecraft/world/item/crafting/Recipe;)Z", cancellable = true, at = @At(value = "HEAD"))
+    public void hookDisableFire(Recipe<?> pRecipe, CallbackInfoReturnable<Boolean> cir) {
+        try {
+            cir.setReturnValue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Inject(method = "contains(Lnet/minecraft/resources/ResourceLocation;)Z", cancellable = true, at = @At(value = "HEAD"))
+    public void hookDisableFire(ResourceLocation pRecipeId, CallbackInfoReturnable<Boolean> cir) {
+        try {
+            cir.setReturnValue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+     */
+}
