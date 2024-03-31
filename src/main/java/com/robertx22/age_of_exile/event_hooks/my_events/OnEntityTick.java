f7f4343dd8c3b8882908ad4d19dd8a4a33d0e119
abandoncaptian <bobert98h@gmail.com>
2024-03-31 14:51:32 -0400

    PR Stat Tracking

--- src/main/java/com/robertx22/age_of_exile/event_hooks/my_events/OnEntityTick.java
+++ src/main/java/com/robertx22/age_of_exile/event_hooks/my_events/OnEntityTick.java
@@ -2,9 +2,12 @@ package com.robertx22.age_of_exile.event_hooks.my_events;
 
 import com.robertx22.age_of_exile.capability.bases.EntityGears;
 import com.robertx22.age_of_exile.capability.entity.EntityData;
+import com.robertx22.age_of_exile.characters.PlayerStats;
+import com.robertx22.age_of_exile.saveclasses.unit.ResourceType;
 import com.robertx22.age_of_exile.uncommon.datasaving.Load;
 import com.robertx22.library_of_exile.events.base.EventConsumer;
 import com.robertx22.library_of_exile.events.base.ExileEvents;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -51,6 +54,30 @@ public class OnEntityTick extends EventConsumer<ExileEvents.OnEntityTick> {
             // todo lets see if this works fine, no need to lag if mobs anyway recalculate stats when needed
             if (entity instanceof Player) {
                 checkGearChanged(entity);
+
+                if (entity.tickCount % 100 == 0) {
+                    Player p = (Player) entity;
+
+                    int max = Math.round(data.getResources().getMax(entity, ResourceType.blood));
+                    p.resetStat(Stats.CUSTOM.get(PlayerStats.TOTAL_BLOOD));
+                    p.awardStat(Stats.CUSTOM.get(PlayerStats.TOTAL_BLOOD), max);
+
+                    max = Math.round(data.getResources().getMax(entity, ResourceType.mana));
+                    p.resetStat(Stats.CUSTOM.get(PlayerStats.TOTAL_MANA));
+                    p.awardStat(Stats.CUSTOM.get(PlayerStats.TOTAL_MANA), max);
+
+                    max = Math.round(data.getResources().getMax(entity, ResourceType.energy));
+                    p.resetStat(Stats.CUSTOM.get(PlayerStats.TOTAL_ENERGY));
+                    p.awardStat(Stats.CUSTOM.get(PlayerStats.TOTAL_ENERGY), max);
+
+                    max = Math.round(data.getResources().getMax(entity, ResourceType.magic_shield));
+                    p.resetStat(Stats.CUSTOM.get(PlayerStats.TOTAL_MAGIC_SHIELD));
+                    p.awardStat(Stats.CUSTOM.get(PlayerStats.TOTAL_MAGIC_SHIELD), max);
+
+                    max = Math.round(data.getResources().getMax(entity, ResourceType.health));
+                    p.resetStat(Stats.CUSTOM.get(PlayerStats.TOTAL_HEALTH));
+                    p.awardStat(Stats.CUSTOM.get(PlayerStats.TOTAL_HEALTH), max);
+                }
             }
 
 
