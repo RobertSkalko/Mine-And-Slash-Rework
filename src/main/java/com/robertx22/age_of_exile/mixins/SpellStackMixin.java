ed54f8062e4cceed14138ed8d21d62a4c527bae3
Robert Skalko <treborx555@gmail.com>
2024-05-29 23:01:53 +0200

    v.5.5.5

--- src/main/java/com/robertx22/age_of_exile/mixins/SpellStackMixin.java
+++
@@ -1,8 +0,0 @@
-package com.robertx22.age_of_exile.mixins;
-
-import net.minecraft.world.item.ItemStack;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ItemStack.class)
-public class SpellStackMixin {
-}
