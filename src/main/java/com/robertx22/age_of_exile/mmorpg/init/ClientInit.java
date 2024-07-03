cd9ca31d806a36632a62894fad1e3da37f9258aa
TUsama <1716976420@qq.com>
2024-07-03 14:30:13 +0800

    fix a IndexOutOfBoundsException

--- src/main/java/com/robertx22/age_of_exile/mmorpg/init/ClientInit.java
+++ src/main/java/com/robertx22/age_of_exile/mmorpg/init/ClientInit.java
@@ -23,10 +23,7 @@ import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class ClientInit {
 
@@ -80,14 +77,11 @@ public class ClientInit {
                     if (!gems.isEmpty()) {
 
                         List<Either<FormattedText, TooltipComponent>> list = x.getTooltipElements();
-                        int rmvIdx = -1;
-                        for (int i = 0; i < list.size(); i++) {
+                        for (int i = 0; i < list.size() && e < gems.size(); i++) {
                             Optional<FormattedText> o = list.get(i).left();
                             if (o.isPresent() && o.get() instanceof Component comp && comp.getContents() instanceof LiteralContents tc) {
                                 if (tc.text().contains("[SOCKET_PLACEHOLDER]")) {
-                                    rmvIdx = i;
-                                    list.remove(i);
-                                    x.getTooltipElements().add(rmvIdx, Either.right(new SocketTooltip.SocketComponent(x.getItemStack(), Arrays.asList(gems.get(e)))));
+                                    list.set(i, Either.right(new SocketTooltip.SocketComponent(x.getItemStack(), Collections.singletonList(gems.get(e)))));
                                     e++;
 
                                 }
