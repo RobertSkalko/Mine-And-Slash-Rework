c029e5fbde77b9a0d03b5309acbd9d4cf2caac90
TUsama <1716976420@qq.com>
2024-07-14 23:35:24 +0800

    salvageblock show only show when shift pressed

--- src/main/java/com/robertx22/age_of_exile/gui/texts/textblocks/SalvageBlock.java
+++ src/main/java/com/robertx22/age_of_exile/gui/texts/textblocks/SalvageBlock.java
@@ -1,6 +1,7 @@
 package com.robertx22.age_of_exile.gui.texts.textblocks;
 
 import com.robertx22.age_of_exile.gui.texts.ExileTooltips;
+import com.robertx22.age_of_exile.saveclasses.gearitem.gear_bases.TooltipInfo;
 import com.robertx22.age_of_exile.uncommon.interfaces.data_items.ISalvagable;
 import com.robertx22.age_of_exile.uncommon.localization.Words;
 import net.minecraft.network.chat.Component;
@@ -13,13 +14,18 @@ public class SalvageBlock extends AbstractTextBlock{
 
     private final ISalvagable data;
 
+    private final TooltipInfo info = new TooltipInfo();
+
     public SalvageBlock(ISalvagable data) {
         this.data = data;
     }
 
     @Override
     public List<? extends Component> getAvailableComponents() {
-        return Collections.singletonList(data.isSalvagable() ? Words.SALVAGEABLE.locName() : Words.UNSALVAGEABLE.locName());
+        if (info.useInDepthStats()){
+            return Collections.singletonList(data.isSalvagable() ? Words.SALVAGEABLE.locName() : Words.UNSALVAGEABLE.locName());
+        }
+        return Collections.emptyList();
     }
 
     @Override
