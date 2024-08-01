2496cf5b5358c02ec7b525d1f735c9b967afd1cd
TUsama <1716976420@qq.com>
2024-08-02 00:28:45 +0800

    introduce PerkButtonPainter, paint the three icon layers to one image

--- src/main/java/com/robertx22/age_of_exile/gui/screens/skill_tree/buttons/PerkPointPair.java
+++ src/main/java/com/robertx22/age_of_exile/gui/screens/skill_tree/buttons/PerkPointPair.java
@@ -2,13 +2,7 @@ package com.robertx22.age_of_exile.gui.screens.skill_tree.buttons;
 
 import com.robertx22.age_of_exile.saveclasses.PointData;
 
-public class PerkPointPair {
-    public PointData perk1, perk2;
-
-    public PerkPointPair(PointData perk1, PointData perk2) {
-        this.perk1 = perk1;
-        this.perk2 = perk2;
-    }
+public record PerkPointPair(PointData perk1, PointData perk2) {
 
     @Override
     public boolean equals(Object o) {
@@ -20,7 +14,15 @@ public class PerkPointPair {
 
     @Override
     public int hashCode() {
-        return perk1.hashCode() + perk2.hashCode();
+        int hashA = perk1.hashCode();
+        int hashB = perk2.hashCode();
+
+        long result = 17;
+        result = 31 * result + (hashA ^ hashB);
+        result = 31 * result + (hashA & hashB);
+        result = 31 * result + (hashA | hashB);
+
+        return (int) result;
     }
 
 }
