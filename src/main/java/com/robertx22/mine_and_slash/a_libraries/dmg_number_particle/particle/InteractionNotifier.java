07a3fcd959eb374b37e917b219a9af696f16a5d3
TUsama <1716976420@qq.com>
2024-10-29 19:36:29 +0800

    use old config option to control the particle notifier

--- src/main/java/com/robertx22/mine_and_slash/a_libraries/dmg_number_particle/particle/InteractionNotifier.java
+++ src/main/java/com/robertx22/mine_and_slash/a_libraries/dmg_number_particle/particle/InteractionNotifier.java
@@ -1,6 +1,7 @@
 package com.robertx22.mine_and_slash.a_libraries.dmg_number_particle.particle;
 
 import com.robertx22.library_of_exile.main.Packets;
+import com.robertx22.mine_and_slash.config.forge.ClientConfigs;
 import com.robertx22.mine_and_slash.vanilla_mc.packets.interaction.ExileInteractionResultPacket;
 import com.robertx22.mine_and_slash.vanilla_mc.packets.interaction.IParticleSpawnMaterial;
 import net.minecraft.server.level.ServerPlayer;
@@ -9,6 +10,7 @@ import net.minecraft.world.entity.LivingEntity;
 public class InteractionNotifier {
 
     public static void notifyClient(IParticleSpawnMaterial notifier, ServerPlayer source, LivingEntity target){
+        if (!ClientConfigs.getConfig().ENABLE_FLOATING_DMG.get()) return;
         Packets.sendToClient(source, new ExileInteractionResultPacket(target.getId(), notifier));
     }
 
