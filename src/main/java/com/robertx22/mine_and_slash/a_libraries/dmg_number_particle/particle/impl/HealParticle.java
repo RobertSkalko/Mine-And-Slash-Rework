85f64496e16aaf79ea64ae77092c7a261146dfa0
TUsama <1716976420@qq.com>
2024-11-01 19:11:00 +0800

    Update HealParticle.java

--- src/main/java/com/robertx22/mine_and_slash/a_libraries/dmg_number_particle/particle/impl/HealParticle.java
+++ src/main/java/com/robertx22/mine_and_slash/a_libraries/dmg_number_particle/particle/impl/HealParticle.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.robertx22.mine_and_slash.a_libraries.dmg_number_particle.particle.ExileInteractionResultParticle;
 import com.robertx22.mine_and_slash.a_libraries.dmg_number_particle.particle.style.IParticleRenderMaterial;
 import com.robertx22.mine_and_slash.a_libraries.dmg_number_particle.particle.style.IParticleRenderStrategy;
+import com.robertx22.mine_and_slash.uncommon.utilityclasses.NumberUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
@@ -25,7 +26,7 @@ public class HealParticle extends ExileInteractionResultParticle {
         posestack.pushPose();
 
         super.getStrategy().setupParticle(this, vertexConsumer, camera, partialTick, posestack);
-        super.getStrategy().renderHeal(this, vertexConsumer, camera, partialTick, posestack, new IParticleRenderMaterial.simpleText(amount + ""));
+        super.getStrategy().renderHeal(this, vertexConsumer, camera, partialTick, posestack, new IParticleRenderMaterial.simpleText(NumberUtils.format(amount)));
         super.getStrategy().changeScale(this, getAge(), getLiftTime(), partialTick);
 
         posestack.popPose();
