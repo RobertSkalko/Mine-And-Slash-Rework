e4326a74d8a99fac96f077db57fc2c779f795b5b
SaloEater <danil.lazarev@gmail.com>
2025-08-05 18:44:04 -0500

    Make expire action to reduce minions counter

--- src/main/java/com/robertx22/mine_and_slash/database/data/spells/components/actions/ExpireAction.java
+++ src/main/java/com/robertx22/mine_and_slash/database/data/spells/components/actions/ExpireAction.java
@@ -2,6 +2,9 @@ package com.robertx22.mine_and_slash.database.data.spells.components.actions;
 
 import com.robertx22.mine_and_slash.database.data.spells.components.MapHolder;
 import com.robertx22.mine_and_slash.database.data.spells.spell_classes.SpellCtx;
+import com.robertx22.mine_and_slash.database.data.spells.summons.entity.SummonEntity;
+import com.robertx22.mine_and_slash.uncommon.datasaving.Load;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 
@@ -16,12 +19,15 @@ public class ExpireAction extends SpellAction {
 
     @Override
     public void tryActivate(Collection<LivingEntity> targets, SpellCtx ctx, MapHolder data) {
-        if (ctx.getPositionEntity() != null) {
-            if (!ctx.getPositionEntity().isRemoved()) {
-
-                // todo this is confusing
-                if (ctx.getPositionEntity() instanceof Player == false) {
-                    ctx.getPositionEntity().discard(); // this can cause infi loops and even calling expire spell multiple times
+        Entity entityToDiscard = ctx.getPositionEntity();
+        if (entityToDiscard != null) {
+            if (!entityToDiscard.isRemoved()) {
+                if (!(entityToDiscard instanceof Player)) {
+                    if (entityToDiscard instanceof SummonEntity summonEntity) {
+                        Load.Unit(summonEntity).summonedPetData.discard((LivingEntity) entityToDiscard);
+                    } else {
+                        entityToDiscard.discard(); // this can cause infi loops and even calling expire spell multiple times
+                    }
                 }
             }
         }
